db94a6a658feb85359bf1d8b87a0125942627846
coursar <coursar@gmail.com>
2021-08-30 21:30:48 +0300

    final project

---
+++ src/main/java/org/example/domain/Card.java
@@ -0,0 +1,13 @@
+package org.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Card {
+  private int id;
+  private String name;
+}
